97fcce5e9d7ea724af94e103222cfc2d71162609
Milos Zivlak <zivlakmilos@gmail.com>
2023-07-06 04:07:12 +0200

    Add todos

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,28 @@ import { createSignal, type Component, Show, For } from 'solid-js';
 
 let counter = 0;
 
+const ESCAPE_KEY = 27;
+const ENTER_KEY = 13;
+
+type Todo = {
+  id: number,
+  title: string,
+  completed: boolean,
+}
+
 const App: Component = () => {
-  const [todos, setTodos] = createSignal([
-    {
-      id: 1,
-      title: 'Learn SolidJS',
-      completed: false,
-    },
-  ]);
+  const [todos, setTodos] = createSignal<Todo[]>([]);
+
+  const addTodo = (event: any) => {
+    const title = event.target.value.trim();
+    if (event.keyCode === ENTER_KEY && title) {
+      setTodos(todos => [
+        ...todos,
+        { id: counter++, title, completed: false }
+      ]);
+      event.target.value = '';
+    }
+  }
 
   return (
     <section class="todoapp">
@@ -17,7 +31,8 @@ const App: Component = () => {
         <h1>Todos</h1>
         <input
           class="new-todo"
-          placeholder="What needs to be done?" />
+          placeholder="What needs to be done?"
+          onKeyDown={addTodo} />
       </header>
       <Show when={todos().length > 0}>
         <ul class="todo-list">
