4c7f631513b00c3d47dd293a897a0c2fefa3884f
Milos Zivlak <zivlakmilos@gmail.com>
2023-07-06 03:39:11 +0200

    Basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import type { Component } from 'solid-js';
+import { createSignal, type Component, Show, For } from 'solid-js';
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+let counter = 0;
 
 const App: Component = () => {
+  const [todos, setTodos] = createSignal([
+    {
+      id: 1,
+      title: 'Learn SolidJS',
+      completed: false,
+    },
+  ]);
+
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+    <section class="todoapp">
+      <header class="header">
+        <h1>Todos</h1>
+        <input
+          class="new-todo"
+          placeholder="What needs to be done?" />
       </header>
-    </div>
+      <Show when={todos().length > 0}>
+        <ul class="todo-list">
+          <For each={todos()}>
+            {
+              todo => (
+                <li class="todo">
+                  <div class="view">
+                    <input type="checkbox" class="toggle" />
+                    <label>{todo.title}</label>
+                    <button class="destroy" />
+                  </div>
+                </li>
+              )
+            }
+          </For>
+        </ul>
+      </Show>
+    </section>
   );
 };
 
